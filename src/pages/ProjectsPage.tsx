45bcaee185fb79c027c5a549bff88e11852cae29
Daniel Wijaya <wijayadaniel19@gmail.com>
2025-09-22 00:20:09 +0700

    update navbar

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -6,7 +6,7 @@ const ProjectsPage = () => {
     return (
         <>
             <Header />
-            <main className="py-10">
+            <main className="py-20">
                 <CMSList collection="projects" h1={true} />
             </main>
             <Footer />
