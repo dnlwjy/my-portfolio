63eac5692e507dfba8372f2313db8e424e7ce859
Daniel Wijaya <wijayadaniel19@gmail.com>
2025-10-05 02:32:11 +0700

    update navbar

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Button from "@/components/ui/Button";
+import AnimationGroup from "@/components/ui/AnimationGroup";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
@@ -12,16 +13,21 @@ const NotFoundPage = () => {
       <main>
         <section className="h-screen flex items-center justify-center w-full">
           <div className="flex flex-col gap-8 text-center w-full justify-center items-center max-w-[640px]">
-            <div className="flex flex-col gap-4">
+            <AnimationGroup
+              delay={300}
+              className="flex flex-col gap-4">
               <h1>I lost this page.</h1>
               <p>I searched high and low but couldn't find what you're looking for. Let's find a better place for you to go.</p>
-            </div>
+            </AnimationGroup>
+
+            <AnimationGroup
+              delay={600}>
             <Button
-              variant="primary"
               onClick={() => navigate("/")}
             >
               Go home
             </Button>
+            </AnimationGroup>
           </div>
         </section>
       </main>
