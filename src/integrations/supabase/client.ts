2661798e26fb1c03fa44c37de4e03f0759b2b6c9
Daniel Wijaya <111511542+dnlwjy@users.noreply.github.com>
2025-10-19 19:42:46 +0700

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,4 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY as string;
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
-
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
