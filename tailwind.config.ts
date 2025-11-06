47abb176a188f4bd969e4edd758f93e3a3b46427
Daniel Wijaya <wijayadaniel19@gmail.com>
2025-11-06 22:41:49 +0700

    update navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,7 +30,7 @@ export default {
 				black: '#121212',
 				white: '#ffffff',
 				gray: '#8D8D8D',
-				darkgray: '#252525',
+				darkgray: '#292929',
 				blue: '#3B82F6'
 			},
 			keyframes: {
