bc1bedb69b02d5614138ebb953f71011bfdf359a
Daniel Wijaya <wijayadaniel19@gmail.com>
2026-04-20 23:46:42 +0700

    Update Sanity Studio schema/config

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -3,5 +3,6 @@ import { stack } from "./mystacks";
 import { shop } from "./shop";
 import { setup } from "./desksetup";
 import { faq } from "./faq";
+import { personalInfo } from "./personal-info";
 
-export const schemaTypes = [projects, stack, shop, setup, faq];
+export const schemaTypes = [projects, stack, shop, setup, faq, personalInfo];
